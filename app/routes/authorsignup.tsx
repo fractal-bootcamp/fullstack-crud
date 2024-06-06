fb2dbb5035c8baeb49dea0afcc31a7fa8d181cea
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-06 16:04:28 -0400

    basic skeleton

---
+++ app/routes/authorsignup.tsx
@@ -0,0 +1,27 @@
+export default function AuthorSignUp() {
+    return (
+        <div className="flex justify-center items-center" >
+
+            {/* form box */}
+            <div className="flex border border-solid w-1/2 h-96 justify-center">
+
+                <div className="flex flex-col justify-center items-center">
+                    <div>
+                        Welcome, author! Sign up.
+                    </div>
+                    <div>
+
+                    </div>
+                    <p>hello</p>
+                </div>
+
+
+
+
+
+            </div>
+        </div >
+    )
+
+}
+
