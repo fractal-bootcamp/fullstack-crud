eaf9b8b653d1a461e8265199fe7f109b0549a5c3
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-06 16:45:23 -0400

    basic form setup

--- app/routes/authorsignup.tsx
+++ app/routes/authorsignup.tsx
@@ -1,25 +1,49 @@
+
+
+
+
+
+
 export default function AuthorSignUp() {
     return (
-        <div className="flex justify-center items-center" >
+        <div className="flex justify-center items-start" >
 
             {/* form box */}
-            <div className="flex border border-solid w-1/2 h-96 justify-center">
+            <div className="flex flex-col border border-solid w-3/4 gap-10 h-96 justify-center items-start">
+                {/* header */}
+                <div className="items-start">
+                    <h1 className="text-2xl font-bold"> Author Signup </h1>
 
-                <div className="flex flex-col justify-center items-center">
-                    <div>
-                        Welcome, author! Sign up.
-                    </div>
-                    <div>
-
-                    </div>
-                    <p>hello</p>
                 </div>
 
+                {/* body */}
+                <div className="flex flex-col items-center justify-start ">
+
+                    <div className="items-center">
+                        Name
+                    </div>
+                    <input className="border-solid border-4">
+                    </input>
+
+                    <div className="items-center">
+                        Biography
+                    </div>
+                    <input className="border-solid border-4">
+                    </input>
+
+
+                    <div className="justify-center">
+
+                        <button className="border-solid border-4">
+                            Submit
+                        </button>
+                    </div>
 
 
 
-
+                </div>
             </div>
+
         </div >
     )
 
